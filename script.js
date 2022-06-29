55483b7470ef82a81b16c50a2b84c20bf74e4083
onye <lasdodis@gmail.com>
2022-06-28 23:32:31 -0500

    finished the base for the javascript but working on displaying operator bug

--- script.js
+++ script.js
@@ -1,6 +1,99 @@
-function input () {
-     
+let currentNumber = ''
+let previousNumber = ''
+let currentOperator = null
+let screenReset = false
+
+
+
+
+const numbers = document.querySelectorAll('.btn')
+const operators = document.querySelectorAll('.btn-operation')
+const equals = document.querySelector('.btn-equals')
+const clearA = document.querySelector('#clear')
+const deleteBtn = document.querySelector('#delete')
+const past= document.querySelector('.past')
+const present = document.querySelector('.present')
+const decimal = document.querySelector('.btn-decimal')
+
+
+
+
+equals.addEventListener('click', evaluate)
+clearA.addEventListener('click', clear)
+deleteBtn.addEventListener('click', deleteNumber)
+decimal.addEventListener('click', appendPoint)
+
+numbers.forEach((button) =>
+  button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operators.forEach((button) =>
+  button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+function appendNumber(number) {
+  if (present.textContent === '0' || screenReset)
+    resetScreen()
+  present.textContent += number
 }
+
+function resetScreen() {
+  present.textContent = ''
+  screenReset = false
+}
+
+function clear() {
+  present.textContent = '0'
+  past.textContent = ''
+  currentNumber = ''
+  previousNumber = ''
+  present = null
+}
+
+function appendPoint() {
+  if (screenReset) resetScreen()
+  if (present.textContent === '')
+    present.textContent = '0'
+  if (present.textContent.includes('.')) return
+  present.textContent += '.'
+}
+
+function deleteNumber() {
+  present.textContent = present.textContent
+    .toString()
+    .slice(0, -1)
+}
+
+function setOperation(operator) {
+  if (currentOperator !== null) evaluate()
+  currentNumber = present.textContent
+  currentOperator = operator
+  past.textContent = `${currentNumber} ${present}`
+  screenReset = true
+}
+
+function evaluate() {
+  if (present === null || screenReset) return
+  if (present === '/' && present.textContent === '0') {
+    alert("You can't divide by 0!")
+    return
+  }
+  previousNumber = present.textContent
+  present.textContent = roundResult(
+    operate(present, currentNumber, previousNumber)
+  )
+  past.textContent = `${currentNumber} ${present} ${previousNumber} =`
+  currentOperator = null
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000
+}
+
+
+
+
+
 
 
 
@@ -19,7 +112,7 @@ function multiply (a , b) {
      function divide (a , b) {
         return a / b
    };
-      function operate (operate , a , b ){
+      function operate (operator , a , b ){
           a = Number(a)
           b = Number(b)
           switch (operator) {
@@ -29,7 +122,7 @@ function multiply (a , b) {
               return subtract(a, b)
             case 'x':
               return multiply(a, b)
-            case '÷':
+            case '/':
               if (b === 0) return null
               else return divide(a, b)
             default:
